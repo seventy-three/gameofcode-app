2dc689bc38199efb66f8878986555b6e636cddcd
Florian Sey <florian.sey@gmail.com>
2016-04-09 15:08:04 +0200

    BusService interface

--- gameofcode/data/src/main/java/lu/ing/gameofcode/services/BusService.java
+++ gameofcode/data/src/main/java/lu/ing/gameofcode/services/BusService.java
@@ -1,12 +1,13 @@
 package lu.ing.gameofcode.services;
 
+import com.google.android.gms.maps.model.LatLng;
+
 import java.util.List;
 
 import lu.ing.gameofcode.model.BusStop;
 
 public interface BusService {
 
-    List<BusStop> getBusStops(final int busLineNumber);
-
+    List<BusStop> getBusStops(final int busLineNumber, LatLng start, LatLng stop);
 
 }
