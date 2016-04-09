f7db65bc03ca1454165c060b1a833a1bcb0106d5
julienH <hablot@gmail.com>
2016-04-09 15:53:58 +0200

    get available station bikes

--- gameofcode/data/src/main/java/lu/ing/gameofcode/line/VelohAlternative.java
+++ gameofcode/data/src/main/java/lu/ing/gameofcode/line/VelohAlternative.java
@@ -15,14 +15,22 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
 public class VelohAlternative {
 
     private static final String API_KEY = "9b84de9ed23056f22bb009f57972fa76bf656360";
+
+    private static final String END_POINT = "https://api.jcdecaux.com/vls/v1/stations/%NUM%?contract=Luxembourg&apiKey="+API_KEY;
 
     private Context context;
     private static final Gson GSON = new Gson();
@@ -42,6 +50,61 @@ public class VelohAlternative {
      */
     public VelohAlternative(Context context) {
         this.context = context;
+    }
+
+    /**
+     * Prepare the API request.
+     *
+     * @param num id of the station
+     * @return
+     */
+    private Request buildRequest(final String num) {
+        return new Request.Builder()
+                .url(END_POINT.replaceAll("%NUM%",num))
+                .build();
+    }
+
+    /**
+     * Return a list of stopId in the a perimeter.
+     *
+     * @param client instance of an OkHttpClient
+     * @param num id of the station
+     * @return true id bike and stand are available, else false
+     * @throws Exception
+     */
+    private boolean getAvailableBike(OkHttpClient client, final String num) throws Exception {
+
+        boolean available = false;
+        Request request;
+        Response response;
+        ResponseBody body;
+        Reader charStream;
+
+        // prepare the request
+        request = buildRequest(num);
+
+        // Execute the request and retrieve the response.
+        response = client.newCall(request).execute();
+        if (response.code()!=404) {
+
+            // Deserialize HTTP response to concrete type.
+            body = response.body();
+            charStream = body.charStream();
+            GsonBuilder gsonBuilder = new GsonBuilder();
+            Type VelohStationJsonDataType = new TypeToken<VelohStationBean>() {
+            }.getType();
+            final Gson gson = gsonBuilder.create();
+
+            // station, bike and stand are available ?
+            final VelohStationBean station = gson.fromJson(charStream, VelohStationJsonDataType);
+            if (null!=station){
+                System.out.println("station n°="+num+" status="+station.getStatus()+" available bike="+station.getAvailable_bikes()+" available stand="+station.getAvailable_bike_stands());
+                available = ("OPEN".equals(station.getStatus()) &&
+                        null!=station.getAvailable_bikes() && station.getAvailable_bikes()>0 &&
+                        null!=station.getAvailable_bike_stands() && station.getAvailable_bike_stands()>0);
+            }
+        }
+        return available;
     }
 
     /**
@@ -85,6 +148,8 @@ public class VelohAlternative {
      */
     public VelohStationBean[] getAvailableVeloh(final String latitude, final String longitude) throws Exception {
 
+        OkHttpClient client = new OkHttpClient();
+
         InputStream inputStream = context.getResources().getAssets().open("VelohStation.json");
         Reader reader = new InputStreamReader(inputStream);
         GsonBuilder gsonBuilder = new GsonBuilder();
@@ -95,7 +160,7 @@ public class VelohAlternative {
         final Map<String, VelohStationBean> mStations = new HashMap<>();
         for (final VelohStationBean station : stationList) {
             double dist = distanceTo(latitude,longitude,station.getLatitude(),station.getLongitude());
-            if (dist<=DIST_MAX){
+            if (dist<=DIST_MAX && getAvailableBike(client,station.getNumber())) {
                 mStations.put(station.getNumber(), station);
             }
         }
--- gameofcode/data/src/main/java/lu/ing/gameofcode/line/VelohStationBean.java
+++ gameofcode/data/src/main/java/lu/ing/gameofcode/line/VelohStationBean.java
@@ -10,6 +10,10 @@ public class VelohStationBean {
     private double latitude;
     private double longitude;
 
+    private String status;
+    private Integer available_bike_stands;
+    private Integer available_bikes;
+
     public String getNumber() {
         return number;
     }
@@ -48,5 +52,29 @@ public class VelohStationBean {
 
     public void setLongitude(double longitude) {
         this.longitude = longitude;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Integer getAvailable_bike_stands() {
+        return available_bike_stands;
+    }
+
+    public void setAvailable_bike_stands(Integer available_bike_stands) {
+        this.available_bike_stands = available_bike_stands;
+    }
+
+    public Integer getAvailable_bikes() {
+        return available_bikes;
+    }
+
+    public void setAvailable_bikes(Integer available_bikes) {
+        this.available_bikes = available_bikes;
     }
 }
